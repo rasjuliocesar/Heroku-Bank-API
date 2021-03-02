29f371e32787d33efbdf79efb79d366ceb8f03c2
Julio Cesar <rasjuliocesar@gmail.com>
2021-03-02 20:07:26 -0300

    Project Created

---
+++ src/main/java/com/herokuBankApi/HerokuBankApiApplication.java
@@ -0,0 +1,13 @@
+package com.herokuBankApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuBankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herokuBankApi/HerokuBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herokuBankApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
