1a74d9068d04f67af8de1f6ab085edcadd3cce7f
Julio Cesar <rasjuliocesar@gmail.com>
2021-03-05 19:51:23 -0300

    Bank Service and Resource - insert/delete/update

--- src/main/java/com/herokuBankApi/resources/BankResource.java
+++ src/main/java/com/herokuBankApi/resources/BankResource.java
@@ -1,13 +1,19 @@
 package com.herokuBankApi.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.herokuBankApi.entities.Bank;
 import com.herokuBankApi.services.BankService;
@@ -28,6 +34,25 @@ public class BankResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Bank> findById(@PathVariable Long id) {
 		Bank bank = bankService.findById(id);
+		return ResponseEntity.ok().body(bank);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Bank> insert(@RequestBody Bank bank) {
+		bank = bankService.insert(bank);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(bank.getId()).toUri();
+		return ResponseEntity.created(uri).body(bank);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		bankService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Bank> update(@PathVariable Long id, @RequestBody Bank bank) {
+		bank = bankService.update(id, bank);
 		return ResponseEntity.ok().body(bank);
 	}
 }
--- src/main/java/com/herokuBankApi/services/BankService.java
+++ src/main/java/com/herokuBankApi/services/BankService.java
@@ -1,5 +1,6 @@
 package com.herokuBankApi.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,5 +23,28 @@ public class BankService {
 	public Bank findById(Long id) {
 		Optional<Bank> bank = bankRepository.findById(id);
 		return bank.get();
+	}
+	
+	public Bank insert(Bank bank) {
+		bank.setCreatedAt(Instant.now());
+		bank.setUpdateAt(Instant.now());
+		bank.setAccountValue(0.0);
+		return bankRepository.save(bank);
+	}
+	
+	public void delete(Long id) {
+		bankRepository.deleteById(id);
+	}
+	
+	public Bank update(Long id, Bank bank) {
+		Bank obj = bankRepository.getOne(id);
+		updateData(obj, bank);
+		return bankRepository.save(obj);
+	}
+
+	private void updateData(Bank obj, Bank bank) {
+		obj.setAccountValue(bank.getAccountValue());
+		obj.setUpdateAt(Instant.now());
+		
 	}
 }
