a32b62aa534b98cae9a46937a498299c323af5ae
Julio Cesar <rasjuliocesar@gmail.com>
2021-03-05 20:16:37 -0300

    Person Service and Resource - insert/delete/update

--- src/main/java/com/herokuBankApi/resources/PersonResource.java
+++ src/main/java/com/herokuBankApi/resources/PersonResource.java
@@ -1,13 +1,19 @@
 package com.herokuBankApi.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.herokuBankApi.entities.Person;
 import com.herokuBankApi.services.PersonService;
@@ -28,6 +34,25 @@ public class PersonResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Person> findById(@PathVariable Long id) {
 		Person person = personService.findById(id);
+		return ResponseEntity.ok().body(person);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Person> insert(@RequestBody Person person) {
+		person = personService.insert(person);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(person.getId()).toUri();
+		return ResponseEntity.created(uri).body(person);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		personService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Person> update(@PathVariable Long id, @RequestBody Person person) {
+		person = personService.update(id, person);
 		return ResponseEntity.ok().body(person);
 	}
 }
--- src/main/java/com/herokuBankApi/services/PersonService.java
+++ src/main/java/com/herokuBankApi/services/PersonService.java
@@ -1,5 +1,6 @@
 package com.herokuBankApi.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,5 +23,34 @@ public class PersonService {
 	public Person findById(Long id) {
 		Optional<Person> person = personRepository.findById(id);
 		return person.get();
+	}
+	
+	public Person insert(Person person) {
+		person.setCreatedAt(Instant.now());
+		person.setUpdatedAt(Instant.now());
+		return personRepository.save(person);
+	}
+	
+	public void delete(Long id) {
+		personRepository.deleteById(id);
+	}
+	
+	public Person update(Long id, Person person) {
+		Person obj = personRepository.getOne(id);
+		updateData(obj, person);
+		return personRepository.save(obj);
+	}
+
+	private void updateData(Person obj, Person person) {
+		obj.setUpdatedAt(Instant.now());
+		obj.setFullName(person.getFullName());
+		obj.setAddress(person.getAddress());
+		obj.setZipCode(person.getZipCode());
+		obj.setState(person.getState());
+		obj.setCountry(person.getCountry());
+		obj.setBirthDate(person.getBirthDate());
+		obj.setDocument(person.getDocument());
+		obj.setPhone(person.getPhone());
+		obj.setEmail(person.getEmail());
 	}
 }
